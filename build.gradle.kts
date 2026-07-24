4d0d1313b59f9806fd7a1a5f1e67064aa0571f8f
Haidox <haidermartin@outlook.de>
2026-07-24 22:38:39 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,3 @@
 plugins {
-    id("com.android.application")
-}
-
-android {
-    namespace = "de.haidox.normandiereise"
-    compileSdk = 35
-
-    defaultConfig {
-        applicationId = "de.haidox.normandiereise"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    id("com.android.application") version "8.5.2" apply false
 }
